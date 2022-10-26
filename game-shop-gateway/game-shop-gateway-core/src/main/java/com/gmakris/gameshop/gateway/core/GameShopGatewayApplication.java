26aef7bdf074a128051feff857f4ad32cc939893
Khepu <makris.giwrgos1234@gmail.com>
2022-10-26 16:25:04 +0300

    Feature: Add gateway structure

---
+++ game-shop-gateway/game-shop-gateway-core/src/main/java/com/gmakris/gameshop/gateway/core/GameShopGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gmakris.gameshop.gateway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShopGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameShopGatewayApplication.class, args);
+    }
+
+}
