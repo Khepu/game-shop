074d47f7185c114f138c6d337a2ab3da800c52c6
Khepu <makris.giwrgos1234@gmail.com>
2022-10-26 20:41:06 +0300

    Chore: Rename domain module to domain-parent

--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/GenericRepository.java
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/GenericRepository.java
@@ -2,9 +2,9 @@ package com.gmakris.gameshop.gateway.repository;
 
 import java.util.UUID;
 import com.gmakris.gameshop.gateway.entity.model.GenericEntity;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 @NoRepositoryBean
-public interface GenericRepository<Entity extends GenericEntity> extends R2dbcRepository<Entity, UUID> {
+public interface GenericRepository<Entity extends GenericEntity> extends ReactiveCrudRepository<Entity, UUID> {
 }
--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/RepositoryConfiguration.java
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/RepositoryConfiguration.java
@@ -1,7 +1,9 @@
 package com.gmakris.gameshop.gateway.repository;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@Configuration
 @EnableR2dbcRepositories
 public class RepositoryConfiguration {
 }
