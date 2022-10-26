76e8d2dfc155dc185371ac7e266bb78bf837a7be
Khepu <makris.giwrgos1234@gmail.com>
2022-10-26 22:26:01 +0300

    Refactor: Separate pricing from game entity

--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-domain/src/main/resources/db/migration/V202210261650__create_entity_tables.sql
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-domain/src/main/resources/db/migration/V202210261650__create_entity_tables.sql
@@ -2,20 +2,26 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE games
 (
-    id         UUID                              DEFAULT gen_random_uuid() NOT NULL,
+    id         UUID                     NOT NULL DEFAULT gen_random_uuid(),
     name       TEXT                     NOT NULL,
-
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
-    price      DECIMAL                  NOT NULL,
+    PRIMARY KEY (id)
+);
 
-    PRIMARY KEY (id, updated_at)
+CREATE TABLE prices
+(
+    id         UUID                     NOT NULL DEFAULT gen_random_uuid(),
+    game_id    UUID                     NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    value      DECIMAL                  NOT NULL,
+
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE orders
 (
-    id                   UUID                              DEFAULT gen_random_uuid() NOT NULL,
+    id                   UUID                     NOT NULL DEFAULT gen_random_uuid(),
     user_id              UUID                     NOT NULL,
     game_id              UUID                     NOT NULL,
 
@@ -25,18 +31,24 @@ CREATE TABLE orders
     PRIMARY KEY (id)
 );
 
-ALTER TABLE games
-    ADD CONSTRAINT "games_non_negative_price"
-        CHECK ( price >= 0 );
+ALTER TABLE prices
+    ADD CONSTRAINT "fk_prices_game_id_games"
+        FOREIGN KEY (game_id)
+            REFERENCES games (id),
+    ADD CONSTRAINT "unique_prices_game_id_created_at"
+        UNIQUE (game_id, created_at),
+    ADD CONSTRAINT "prices_non_negative_value"
+        CHECK ( value >= 0 );
 
 ALTER TABLE orders
     ADD CONSTRAINT "fk_orders_game_id_games"
         FOREIGN KEY (game_id, game_last_updated_at)
-            REFERENCES games (id, updated_at),
+            REFERENCES games (id),
     ADD CONSTRAINT "unique_orders_user_id_game_id"
         UNIQUE (user_id, game_id);
 
+CREATE INDEX "idx_btree_prices_game_id" ON prices (game_id);
+CREATE UNIQUE INDEX "idx_btree_prices_game_id_created_at" ON prices (game_id, created_at DESC);
+
 CREATE INDEX "idx_btree_orders_user_id" ON orders (user_id);
 CREATE INDEX "idx_btree_orders_game_id" ON orders (game_id);
-
-SELECT create_hypertable('games', 'updated_at');
