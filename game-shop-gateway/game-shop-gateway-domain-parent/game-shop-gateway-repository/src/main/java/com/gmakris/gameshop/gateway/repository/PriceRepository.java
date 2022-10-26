930b0d8e4a2a77e1eb4bd39a520ea2d606eeba4a
Khepu <makris.giwrgos1234@gmail.com>
2022-10-26 22:37:30 +0300

    Refactor: Move price finding query to price repo

--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/PriceRepository.java
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/PriceRepository.java
@@ -1,8 +1,22 @@
 package com.gmakris.gameshop.gateway.repository;
 
+import java.util.UUID;
 import com.gmakris.gameshop.gateway.entity.model.Price;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface PriceRepository extends GenericRepository<Price> {
+
+    @Query("""
+    select *
+    from prices p
+        inner join games g on g.id = p.game_id
+    where g.id = :id
+    order by created_at
+    limit 1
+    """)
+    Mono<Price> findMostRecentPriceByGameId(@Param("id") UUID id);
 }
