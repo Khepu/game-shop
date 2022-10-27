c53dbf0cd74c3ee6e9c6568771599abae82f1187
Khepu <makris.giwrgos1234@gmail.com>
2022-10-28 00:22:36 +0300

    Feature: Add games FTS endpoint

--- game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/controller/GameController.java
+++ game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/controller/GameController.java
@@ -9,6 +9,7 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 
 import com.gmakris.gameshop.gateway.api.ApiProperties;
 import com.gmakris.gameshop.gateway.api.util.ParseUtil;
+import com.gmakris.gameshop.gateway.entity.model.Game;
 import com.gmakris.gameshop.gateway.mapper.GameMapper;
 import com.gmakris.gameshop.gateway.mapper.PriceMapper;
 import com.gmakris.gameshop.gateway.service.crud.GameService;
@@ -47,24 +48,28 @@ public class GameController implements GenericController {
         this.apiProperties = apiProperties;
     }
 
+    private Mono<PricedGameDto> toPricedGame(final Game game) {
+        return priceService.findMostRecentPriceByGame(game)
+            .zipWith(Mono.just(game))
+            .map(priceAndGame -> new PricedGameDto(
+                gameMapper.to(priceAndGame.getT2()),
+                priceMapper.to(priceAndGame.getT1())));
+    }
+
     private Mono<ServerResponse> findAllPaginated(final ServerRequest serverRequest) {
         return Mono.zip(
                 ParseUtil.toInteger(serverRequest.queryParam("size"))
                     .doOnError(throwable -> log.error("Error parsing page size!"))
-                    .onErrorMap(throwable -> new RuntimeException("Error parsing page size!"))
+                    .onErrorMap(throwable -> new RuntimeException("Could not parse query parameter 'size'!"))
                     .map(size -> min(apiProperties.getMaxPageSize(), size)),
                 ParseUtil.toInteger(serverRequest.queryParam("page"))
                     .doOnError(throwable -> log.error("Error parsing page number!"))
-                    .onErrorMap(throwable -> new RuntimeException("Error parsing page number!"))
+                    .onErrorMap(throwable -> new RuntimeException("Could not parse query parameter 'page'!"))
                     .map(page -> max(FIRST_PAGE, page)))
             .flatMapMany(pageAndSize -> gameService.findAllPaginated(
-                    pageAndSize.getT1(),
-                    pageAndSize.getT2())
-                .flatMap(game -> priceService.findMostRecentPriceByGame(game)
-                    .zipWith(Mono.just(game)))
-                .map(priceAndGame -> new PricedGameDto(
-                    gameMapper.to(priceAndGame.getT2()),
-                    priceMapper.to(priceAndGame.getT1()))))
+                pageAndSize.getT1(),
+                pageAndSize.getT2()))
+            .flatMap(this::toPricedGame)
             .collectList()
             .flatMap(pricedGames -> ServerResponse
                 .ok()
@@ -72,6 +77,26 @@ public class GameController implements GenericController {
                 .bodyValue(pricedGames))
             .onErrorResume(throwable -> ServerResponse
                 .status(INTERNAL_SERVER_ERROR)
+                .contentType(APPLICATION_JSON)
+                .bodyValue(throwable.getMessage()));
+    }
+
+    private Mono<ServerResponse> search(final ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(String.class)
+            .flatMapMany(query -> gameService.findPricedGamesByQuery(
+                query,
+                apiProperties.getDefaultPageSize()))
+            .flatMap(this::toPricedGame)
+            .collectList()
+            .flatMap(pricedGames -> ServerResponse
+                .ok()
+                .contentType(APPLICATION_JSON)
+                .bodyValue(pricedGames))
+            .doOnError(throwable -> log.error("Error while searching for games by query!"))
+            .onErrorMap(throwable -> new RuntimeException("Invalid query!"))
+            .onErrorResume(throwable -> ServerResponse
+                .status(INTERNAL_SERVER_ERROR)
+                .contentType(APPLICATION_JSON)
                 .bodyValue(throwable.getMessage()));
     }
 
