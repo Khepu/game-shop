e3c37a8d61aa682647cbcf9643fc9d8c23d259b1
Khepu <makris.giwrgos1234@gmail.com>
2022-10-27 23:57:49 +0300

    Chore: Add core configuration

---
+++ game-shop-gateway/game-shop-gateway-core/src/main/java/com/gmakris/gameshop/gateway/core/CoreConfiguration.java
@@ -0,0 +1,18 @@
+package com.gmakris.gameshop.gateway.core;
+
+import com.gmakris.gameshop.gateway.api.ApiConfiguration;
+import com.gmakris.gameshop.gateway.domain.DomainConfiguration;
+import com.gmakris.gameshop.gateway.repository.RepositoryConfiguration;
+import com.gmakris.gameshop.gateway.service.ServiceConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+    ApiConfiguration.class,
+    DomainConfiguration.class,
+    ServiceConfiguration.class,
+    RepositoryConfiguration.class
+})
+public class CoreConfiguration {
+}
