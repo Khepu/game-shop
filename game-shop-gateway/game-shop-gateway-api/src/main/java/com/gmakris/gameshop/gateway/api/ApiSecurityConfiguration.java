a9067a0e0bf02d440ba722c1ac8aa3d0b798d595
Khepu <makris.giwrgos1234@gmail.com>
2022-10-29 01:29:08 +0300

    Refactor: Fix security config

--- game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/ApiSecurityConfiguration.java
+++ game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/ApiSecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.gmakris.gameshop.gateway.api;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 public class ApiSecurityConfiguration {
 
@@ -22,10 +24,12 @@ public class ApiSecurityConfiguration {
         return http
             .csrf().disable()
             .authorizeExchange()
-            .pathMatchers("/auth/register")
-            .permitAll()
-            .anyExchange().authenticated()
+                .pathMatchers("/auth/register").permitAll()
+                .anyExchange().authenticated()
             .and()
+                .httpBasic()
+            .and()
+                .formLogin().disable()
             .build();
     }
 }
