1e15b6e5c3285f310ce44305df0deed05be492d3
Khepu <makris.giwrgos1234@gmail.com>
2022-10-28 20:46:47 +0300

    Chore: Minor changes

--- game-shop-gateway/game-shop-gateway-core/src/main/java/com/gmakris/gameshop/gateway/core/CoreConfiguration.java
+++ game-shop-gateway/game-shop-gateway-core/src/main/java/com/gmakris/gameshop/gateway/core/CoreConfiguration.java
@@ -2,6 +2,7 @@ package com.gmakris.gameshop.gateway.core;
 
 import com.gmakris.gameshop.gateway.api.ApiConfiguration;
 import com.gmakris.gameshop.gateway.domain.DomainConfiguration;
+import com.gmakris.gameshop.gateway.mapper.MapperConfiguration;
 import com.gmakris.gameshop.gateway.repository.RepositoryConfiguration;
 import com.gmakris.gameshop.gateway.service.ServiceConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({
     ApiConfiguration.class,
+    MapperConfiguration.class,
     DomainConfiguration.class,
     ServiceConfiguration.class,
     RepositoryConfiguration.class
--- game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/ServiceConfiguration.java
+++ game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/ServiceConfiguration.java
@@ -1,10 +1,12 @@
 package com.gmakris.gameshop.gateway.service;
 
 import com.gmakris.gameshop.gateway.repository.RepositoryConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({RepositoryConfiguration.class})
 @Configuration
+@ComponentScan
 public class ServiceConfiguration {
 }
