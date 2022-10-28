2d88e361ccd1c9a80c7c61ece89c9e321d783de0
Khepu <makris.giwrgos1234@gmail.com>
2022-10-28 20:46:17 +0300

    Feature: Validate config on startup

--- game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/ApiProperties.java
+++ game-shop-gateway/game-shop-gateway-api/src/main/java/com/gmakris/gameshop/gateway/api/ApiProperties.java
@@ -1,6 +1,9 @@
 package com.gmakris.gameshop.gateway.api;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.List;
+import javax.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,5 +22,23 @@ public class ApiProperties {
      * Default pagination size.
      */
     private int defaultPageSize = 20;
+
+    /**
+     * Maximum page size allowed.
+     */
     private int maxPageSize = 100;
+
+    @PostConstruct
+    public void validOrThrow() {
+        requireNonNull(allowedOrigins, "api.allowed-origins cannot be null!");
+        requireNonNull(allowedHeaders, "api.allowed-headers cannot be null!");
+
+        if (defaultPageSize <= 0) {
+            throw new RuntimeException("api.default-page-size has to be > 0!");
+        }
+
+        if (maxPageSize <= defaultPageSize) {
+            throw new RuntimeException("api.max-page-size has to be > api.default-page-size!");
+        }
+    }
 }
