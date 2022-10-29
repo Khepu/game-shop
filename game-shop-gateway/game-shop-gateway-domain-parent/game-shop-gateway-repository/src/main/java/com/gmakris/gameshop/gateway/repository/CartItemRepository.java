c8a5e1b2c08d8de482dc6868b7dac6a232501a69
Khepu <makris.giwrgos1234@gmail.com>
2022-10-29 23:35:40 +0300

    Chore: Add missing annotations

--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/CartItemRepository.java
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/CartItemRepository.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 import com.gmakris.gameshop.gateway.entity.model.CartItem;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface CartItemRepository extends GenericRepository<CartItem> {
 
     @Query("""
--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/RepositoryConfiguration.java
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-repository/src/main/java/com/gmakris/gameshop/gateway/repository/RepositoryConfiguration.java
@@ -1,9 +1,11 @@
 package com.gmakris.gameshop.gateway.repository;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
+@ComponentScan
 @EnableR2dbcRepositories
 public class RepositoryConfiguration {
 }
