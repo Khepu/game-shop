b7791f17b2bdf4a9d7faaa9bc1b38468461fc5ef
Khepu <makris.giwrgos1234@gmail.com>
2022-10-29 23:20:11 +0300

    Refactor: Fix indices

--- game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-domain/src/main/resources/db/migration/V202210261650__create_entity_tables.sql
+++ game-shop-gateway/game-shop-gateway-domain-parent/game-shop-gateway-domain/src/main/resources/db/migration/V202210261650__create_entity_tables.sql
@@ -41,6 +41,17 @@ CREATE TABLE users
     PRIMARY KEY (id)
 );
 
+CREATE TABLE cart_items
+(
+    id         UUID                        NOT NULL DEFAULT gen_random_uuid(),
+    game_id    UUID                        NOT NULL,
+    user_id    UUID                        NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+    operation  TEXT                        NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
 ALTER TABLE prices
     ADD CONSTRAINT "fk_prices_game_id_games"
         FOREIGN KEY (game_id)
@@ -60,15 +71,29 @@ ALTER TABLE orders
     ADD CONSTRAINT "unique_orders_user_id_game_id"
         UNIQUE (user_id, game_id);
 
+ALTER TABLE cart_items
+    ADD CONSTRAINT "fk_cart_items_game_id_games"
+        FOREIGN KEY (game_id)
+            REFERENCES games (id),
+    ADD CONSTRAINT "fk_cart_items_user_id_users"
+        FOREIGN KEY (user_id)
+            REFERENCES users (id);
+
 ALTER TABLE users
     ADD CONSTRAINT "unique_users_username"
         UNIQUE (username);
 
-CREATE INDEX "idx_btree_games_created_at" ON games (created_at);
+CREATE INDEX "idx_btree_games_created_at" ON games (created_at DESC);
 CREATE INDEX "idx_gin_games_name" ON games USING gin (name_embeddings);
 
 CREATE INDEX "idx_btree_prices_game_id" ON prices (game_id);
-CREATE UNIQUE INDEX "idx_btree_prices_game_id_created_at" ON prices (game_id, created_at DESC);
+CREATE INDEX "idx_btree_prices_created_at" ON prices (created_at DESC);
 
 CREATE INDEX "idx_btree_orders_user_id" ON orders (user_id);
 CREATE INDEX "idx_btree_orders_game_id" ON orders (game_id);
+
+CREATE INDEX "idx_btree_users_username" ON users (username);
+
+CREATE INDEX "idx_btree_cart_items_game_id" ON cart_items (game_id);
+CREATE INDEX "idx_btree_cart_items_user_id" ON cart_items (user_id);
+CREATE INDEX "idx_btree_cart_items_created_at" ON cart_items (created_at DESC);
