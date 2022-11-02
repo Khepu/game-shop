b01e389b00d6c3848a02369256825134054e2207
Khepu <makris.giwrgos1234@gmail.com>
2022-11-03 00:03:26 +0200

    Feature: Add price publication method

--- game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/crud/backoffice/PublicationService.java
+++ game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/crud/backoffice/PublicationService.java
@@ -1,7 +1,11 @@
 package com.gmakris.gameshop.gateway.service.crud.backoffice;
 
+import java.util.UUID;
+import com.gmakris.gameshop.gateway.entity.model.Price;
 import com.gmakris.gameshop.gateway.entity.model.backoffice.Publication;
 import com.gmakris.gameshop.gateway.service.crud.GenericCrudService;
+import reactor.core.publisher.Mono;
 
 public interface PublicationService extends GenericCrudService<Publication> {
+    Mono<Price> publish(UUID unpublishedPriceId, UUID userId);
 }
--- game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/crud/backoffice/PublicationServiceImpl.java
+++ game-shop-gateway/game-shop-gateway-service/src/main/java/com/gmakris/gameshop/gateway/service/crud/backoffice/PublicationServiceImpl.java
@@ -1,22 +1,52 @@
 package com.gmakris.gameshop.gateway.service.crud.backoffice;
 
+import java.util.UUID;
+import com.gmakris.gameshop.gateway.entity.model.Price;
 import com.gmakris.gameshop.gateway.entity.model.backoffice.Publication;
 import com.gmakris.gameshop.gateway.repository.GenericRepository;
 import com.gmakris.gameshop.gateway.repository.backoffice.PublicationRepository;
 import com.gmakris.gameshop.gateway.service.crud.AbstractCrudService;
+import com.gmakris.gameshop.gateway.service.crud.PriceService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PublicationServiceImpl extends AbstractCrudService<Publication> implements PublicationService {
 
+    private final PriceService priceService;
     private final PublicationRepository repository;
+    private final UnpublishedPriceService unpublishedPriceService;
 
-    public PublicationServiceImpl(final PublicationRepository repository) {
+    public PublicationServiceImpl(
+        final PriceService priceService,
+        final PublicationRepository repository,
+        final UnpublishedPriceService unpublishedPriceService
+    ) {
+        this.priceService = priceService;
         this.repository = repository;
+        this.unpublishedPriceService = unpublishedPriceService;
     }
 
     @Override
     protected GenericRepository<Publication> repository() {
         return repository;
+    }
+
+    @Override
+    public Mono<Price> publish(final UUID unpublishedPriceId, final UUID userId) {
+        return unpublishedPriceService.findOneByIdAndUserId(unpublishedPriceId, userId)
+            .map(unpublishedPrice -> new Price(
+                null,
+                unpublishedPrice.gameId(),
+                null,
+                unpublishedPrice.value()))
+            .flatMap(priceService::save)
+            .flatMap(publishedPrice -> save(
+                new Publication(
+                    null,
+                    unpublishedPriceId,
+                    publishedPrice.id(),
+                    null))
+                .thenReturn(publishedPrice));
     }
 }
